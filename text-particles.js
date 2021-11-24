ca3887646d11dc155e7ab037b1282bc4f1b50d33
Piotr Bania <piotr.bania@europe.com>
2021-11-24 12:27:03 +0000

    init

--- text-particles.js
+++ text-particles.js
@@ -15,5 +15,16 @@ const mouse = {
 window.addEventListener('mousemove', function(event) {
     mouse.x = event.x
     mouse.y = event.y
-    console.log(mouse.x, mouse.y)
 })
+
+// Gradient for text
+
+let gradient = ctx.createLinearGradient(0, 0, 100, 100)
+gradient.addColorStop(0, '#7161F5')
+gradient.addColorStop(1, '#62F5E6')
+
+// Drawing text
+
+ctx.fillStyle = gradient
+ctx.font = '700 50px Expansiva'
+ctx.fillText('Ala ma kota', 0, 40)

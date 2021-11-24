7b5466b27db0e3b4c486aecefde04b4bbc40b8d4
Piotr Bania <piotr.bania@europe.com>
2021-11-24 12:08:48 +0000

    init

---
+++ text-particles.js
@@ -0,0 +1,19 @@
+const canvas = document.getElementById('canvas-1')
+const ctx = canvas.getContext('2d')
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+let particleArray = []
+
+// Mouse interactions
+const mouse = {
+    x: null,
+    y: null,
+    radius: 150
+}
+
+window.addEventListener('mousemove', function(event) {
+    mouse.x = event.x
+    mouse.y = event.y
+    console.log(mouse.x, mouse.y)
+})
